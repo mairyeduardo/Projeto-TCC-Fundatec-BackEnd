4eab253bdecd24abaca7af9852386684396b9d2b
mairyeduardo <mairyeduardo@gmail.com>
2024-02-15 21:52:42 -0300

    alterando nome errado do arquivo

--- src/main/java/br/com/solocraft/controller/ClienteController.java
+++ src/main/java/br/com/solocraft/controller/ClienteController.java
@@ -5,5 +5,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/cliente")
-public class ClienteService {
+public class ClienteController {
 }
